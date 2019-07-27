bff1c768032cd1bdc3805b00f6327434ed4d5c14
wndenis <winda.wzse@gmail.com>
2019-07-28 02:36:45 +0300

    Логин, хранение данных для входа

--- AndroidTuts/tutorial/SimpleRoutingSolution/app/src/main/java/com/mapstutorial/simplerouting/LoginActivity.java
+++ AndroidTuts/tutorial/SimpleRoutingSolution/app/src/main/java/com/mapstutorial/simplerouting/LoginActivity.java
@@ -1,6 +1,116 @@
 package com.mapstutorial.simplerouting;
 
-public class LoginActivity {
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static android.app.PendingIntent.getActivity;
+
+public class LoginActivity extends FragmentActivity {
+    private static final String LOG_TAG = RoutingActivity.class.getSimpleName();
+
+    // permissions request code
+    private final static int REQUEST_CODE_ASK_PERMISSIONS = 1;
+    private String login;
+    private SharedPreferences sharedPref;
+
+    /**
+     * Permissions that need to be explicitly requested from end user.
+     */
+    private static final String[] REQUIRED_SDK_PERMISSIONS = new String[] {
+            Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.WRITE_EXTERNAL_STORAGE };
+
+    protected void checkPermissions() {
+        final List<String> missingPermissions = new ArrayList<String>();
+        // check all required dynamic permissions
+        for (final String permission : REQUIRED_SDK_PERMISSIONS) {
+            final int result = ContextCompat.checkSelfPermission(this, permission);
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                missingPermissions.add(permission);
+            }
+        }
+        if (!missingPermissions.isEmpty()) {
+            // request all missing permissions
+            final String[] permissions = missingPermissions
+                    .toArray(new String[missingPermissions.size()]);
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE_ASK_PERMISSIONS);
+        } else {
+            final int[] grantResults = new int[REQUIRED_SDK_PERMISSIONS.length];
+            Arrays.fill(grantResults, PackageManager.PERMISSION_GRANTED);
+            onRequestPermissionsResult(REQUEST_CODE_ASK_PERMISSIONS, REQUIRED_SDK_PERMISSIONS,
+                    grantResults);
+        }
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        TryLogin();
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS:
+                for (int index = permissions.length - 1; index >= 0; --index) {
+                    if (grantResults[index] != PackageManager.PERMISSION_GRANTED) {
+                        // exit the app if one permission is not granted
+                        Toast.makeText(this, "Required permission '" + permissions[index]
+                                + "' not granted, exiting", Toast.LENGTH_LONG).show();
+                        finish();
+                        return;
+                    }
+                }
+                break;
+        }
+    }
+
+    public void TryLogin(){
+        sharedPref = getSharedPreferences("login_file", Context.MODE_PRIVATE);
+        login = sharedPref.getString("login", "");
+        if (login.equals("")) {
+            Toast.makeText(getApplicationContext(),
+                    "Введите логин", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            StartRouting(login);
+        }
+    }
+
+    public void Login(View view) {
+        login = ((TextView)findViewById(R.id.logineditText)).getText().toString();
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.clear();
+        editor.putString("login", login);
+        editor.apply();
+        TryLogin();
+    }
+
+    private void StartRouting(String login){
+        Intent myIntent = new Intent(this, RoutingActivity.class);
+        startActivity(myIntent);
+        finish();
+    }
+
 
 
 }
--- AndroidTuts/tutorial/SimpleRoutingSolution/app/src/main/java/com/mapstutorial/simplerouting/RoutingActivity.java
+++ AndroidTuts/tutorial/SimpleRoutingSolution/app/src/main/java/com/mapstutorial/simplerouting/RoutingActivity.java
@@ -12,31 +12,20 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import android.Manifest;
-import android.app.Activity;
-import android.os.AsyncTask;
-import android.support.annotation.Nullable;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
 import android.support.v4.app.FragmentActivity;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.here.android.mpa.common.GeoBoundingBox;
 import com.here.android.mpa.common.GeoCoordinate;
@@ -54,15 +43,6 @@ import javax.net.ssl.HttpsURLConnection;
 public class RoutingActivity extends FragmentActivity {
     private static final String LOG_TAG = RoutingActivity.class.getSimpleName();
 
-    // permissions request code
-    private final static int REQUEST_CODE_ASK_PERMISSIONS = 1;
-
-    /**
-     * Permissions that need to be explicitly requested from end user.
-     */
-    private static final String[] REQUIRED_SDK_PERMISSIONS = new String[] {
-            Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.WRITE_EXTERNAL_STORAGE };
-
     // map embedded in the map fragment
     private Map map = null;
 
@@ -75,19 +55,20 @@ public class RoutingActivity extends FragmentActivity {
     // MapRoute for this activity
     private static MapRoute mapRoute = null;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        checkPermissions();
-    }
 
     private SupportMapFragment getSupportMapFragment() {
         return (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapfragment);
     }
 
-    private void initialize() {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialize();
+    }
 
+
+    private void initialize() {
         // Search for the map fragment to finish setup by calling init().
         mapFragment = getSupportMapFragment();
         mapFragment.init(new OnEngineInitListener() {
@@ -109,51 +90,6 @@ public class RoutingActivity extends FragmentActivity {
 
         textViewResult = (TextView) findViewById(R.id.title);
         textViewResult.setText(R.string.textview_routecoordinates_2waypoints);
-    }
-
-    /**
-     * Checks the dynamically controlled permissions and requests missing permissions from end user.
-     */
-    protected void checkPermissions() {
-        final List<String> missingPermissions = new ArrayList<String>();
-        // check all required dynamic permissions
-        for (final String permission : REQUIRED_SDK_PERMISSIONS) {
-            final int result = ContextCompat.checkSelfPermission(this, permission);
-            if (result != PackageManager.PERMISSION_GRANTED) {
-                missingPermissions.add(permission);
-            }
-        }
-        if (!missingPermissions.isEmpty()) {
-            // request all missing permissions
-            final String[] permissions = missingPermissions
-                    .toArray(new String[missingPermissions.size()]);
-            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE_ASK_PERMISSIONS);
-        } else {
-            final int[] grantResults = new int[REQUIRED_SDK_PERMISSIONS.length];
-            Arrays.fill(grantResults, PackageManager.PERMISSION_GRANTED);
-            onRequestPermissionsResult(REQUEST_CODE_ASK_PERMISSIONS, REQUIRED_SDK_PERMISSIONS,
-                    grantResults);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[],
-            @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_CODE_ASK_PERMISSIONS:
-                for (int index = permissions.length - 1; index >= 0; --index) {
-                    if (grantResults[index] != PackageManager.PERMISSION_GRANTED) {
-                        // exit the app if one permission is not granted
-                        Toast.makeText(this, "Required permission '" + permissions[index]
-                                + "' not granted, exiting", Toast.LENGTH_LONG).show();
-                        finish();
-                        return;
-                    }
-                }
-                // all permissions were granted
-                initialize();
-                break;
-        }
     }
 
     @Override
@@ -223,6 +159,15 @@ public class RoutingActivity extends FragmentActivity {
 //        }
     }
 
+    public void Logout(MenuItem item){
+        SharedPreferences.Editor editor = getSharedPreferences("login_file", Context.MODE_PRIVATE).edit();
+        editor.clear();
+        editor.apply();
+
+        Intent myIntent = new Intent(this, LoginActivity.class);
+        startActivity(myIntent);
+        finish();
+    }
     private RouteManager.Listener routeManagerListener = new RouteManager.Listener() {
         public void onCalculateRouteFinished(RouteManager.Error errorCode,
                 List<RouteResult> result) {
